34456beaaf11d8c88dfad2a5a8de2fb522484c2c
Todor Ivanov <ivanov.todor1@gmail.com>
2017-03-03 16:02:33 +0200

    Add pet and user entity

---
+++ app/Entities/Pet.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+
+class Pet extends Eloquent{
+    protected $table = 'user_pet';
+
+    public function pet()
+    {
+        //1 - parameter Entity
+        //2 - forein key
+        //3 - local key
+        return $this->belongsTo('App\Entities\Pet');
+    }
+}
+
