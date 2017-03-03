657847901af29a0cce7bc05c78a0e87db1cbd87e
chicken <g_dobrikova@yahoo.com>
2017-03-03 17:48:05 +0200

    removed brackets

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,13 +9,13 @@
 <head>
     <base href="{{URL::asset('/')}}" target="_top">
 
-    <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css')}}}"/>
-    <link rel="stylesheet" href="{{ URL::asset('font-awesome/4.2.0/css/font-awesome.min.css')}}}"/>
-    <link rel="stylesheet" href="{{ URL::asset('css/theme.min.css') }}}" class="theme-stylesheet" id="theme-style"/>
-    <link rel="stylesheet" href="{{ URL::asset('fonts/fonts.googleapis.com.css')}}}"/>
-    <script type="text/javascript" src="{{ URL::asset('js/jquery.2.1.1.min.js')}}}"></script>
-    <script src="{{ URL::asset('js/bootstrap.min.js')}}}"></script>
-    <script src="{{ URL::asset('js/theme.min.js')}}}"></script>
+    <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css')}}"/>
+    <link rel="stylesheet" href="{{ URL::asset('font-awesome/4.2.0/css/font-awesome.min.css')}}"/>
+    <link rel="stylesheet" href="{{ URL::asset('css/theme.min.css') }}" class="theme-stylesheet" id="theme-style"/>
+    <link rel="stylesheet" href="{{ URL::asset('fonts/fonts.googleapis.com.css')}}"/>
+    <script type="text/javascript" src="{{ URL::asset('js/jquery.2.1.1.min.js')}}"></script>
+    <script src="{{ URL::asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{ URL::asset('js/theme.min.js')}}"></script>
 </head>
 <body class="no-skin">
 <div class="main-container" id="main-container">
