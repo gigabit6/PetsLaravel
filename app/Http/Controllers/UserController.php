b361ab249a0655d611be1f723a0820dc70cafd74
Todor Ivanov <ivanov.todor1@gmail.com>
2017-03-03 16:45:40 +0200

    Fix migrations and add default value for isAdmin

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,20 +10,12 @@ namespace App\Http\Controllers;
 
 class UserController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+
     public function __construct()
     {
+        $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return view('index');
